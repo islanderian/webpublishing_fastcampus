c2eb5263f05bdccdc3a7f3b93d1aff77583f5796
islanderian <createdby.island@gmail.com>
2024-06-26 14:04:28 +0900

    Ch02 - 02 보일러 플레이트 두가지 방법

---
+++ Part2-Canvas/0_Canvas_Boiler Plate/index.js
@@ -0,0 +1,47 @@
+import CanvasOption from "./js/CanvasOption.js";
+
+class Canvas extends CanvasOption {
+  constructor() {
+    super();
+  }
+
+  init() {
+    this.canvasWidth = innerWidth;
+    this.canvasHeight = innerHeight;
+    this.canvas.width = this.canvasWidth * this.dpr;
+    this.canvas.height = this.canvasHeight * this.dpr;
+    this.ctx.scale(this.dpr, this.dpr);
+
+    this.canvas.style.width = canvasWidth + "px";
+    this.canvas.style.height = canvasHeight + "px";
+  }
+
+  render() {
+    let now, delta;
+    let then = Date.now();
+
+    const frame = () => {
+      requestAnimationFrame(frame);
+
+      now = Date.now();
+      delta = now - then;
+      if (delta < this.interval) return;
+      // 이곳부터 작성
+      this.ctx.fillRect(100, 100, 200, 200);
+
+      ////////////////
+      then = now - (delta % this.interval);
+    };
+    requestAnimationFrame(frame);
+  }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener("load", () => {
+  canvas.init();
+  canvas.render();
+});
+window.addEventListener("resize", () => {
+  canvas.init();
+});
