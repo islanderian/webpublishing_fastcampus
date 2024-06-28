7cc3a2674d8149bdf015b338caf449371d584814
islanderian <createdby.island@gmail.com>
2024-06-28 17:21:30 +0900

    Ch03 - 02 기초 뼈대 구성하기

---
+++ Part2-Canvas/Ch03_LG카운트다운/Boiler_Plate/index.js
@@ -0,0 +1,43 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const dpr = window.devicePixelRatio;
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
+
+const fps = 60;
+const interval = 1000 / fps;
+
+function init() {
+  canvasWidth = innerWidth;
+  canvasHeight = innerHeight;
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+}
+function render() {
+  let now, delta;
+  let then = Date.now();
+
+  const frame = () => {
+    requestAnimationFrame(frame);
+    now = Date.now();
+    delta = now - then;
+    if (delta < interval) return;
+
+    console.log("frame");
+
+    then = now - (delta % interval);
+  };
+
+  requestAnimationFrame(frame);
+}
+
+window.addEventListener("load", () => {
+  init();
+  render();
+});
+
+window.addEventListener("resize", init);
