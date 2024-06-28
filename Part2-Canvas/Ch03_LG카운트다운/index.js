5dcde92a7058e37b69d6d74d430dda99a14c99be
islanderian <createdby.island@gmail.com>
2024-06-28 20:36:48 +0900

    Ch03 - 04 파티클 애니메이션 만들기

--- Part2-Canvas/Ch03_LG카운트다운/index.js
+++ Part2-Canvas/Ch03_LG카운트다운/index.js
@@ -23,7 +23,7 @@ function init() {
 }
 
 function createRing() {
-  const PARTICLE_NUM = 20;
+  const PARTICLE_NUM = 800;
 
   for (let i = 0; i < PARTICLE_NUM; i++) {
     particles.push(new Particle());
@@ -39,11 +39,24 @@ function render() {
     now = Date.now();
     delta = now - then;
     if (delta < interval) return;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    particles.forEach((particle, index) => {
-      particle.update();
-      particle.draw(ctx);
-    });
+    // forEach를 사용하면 splice 하면서 생기는 빈 배열 때문에 깜빡거리게 되는 것을 방지하기 위해 거꾸로 순회하는 for문으로 대체함
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].draw(ctx);
+
+      if (particles[i].opacity < 0) particles.splice(i, 1);
+    }
+
+    // particles.forEach((particle, index) => {
+    //   particle.update();
+    //   particle.draw(ctx);
+
+    //   if (particle.opacity < 0) {
+    //     particles.splice(index, 1);
+    //   }
+    // });
 
     then = now - (delta % interval);
   };
