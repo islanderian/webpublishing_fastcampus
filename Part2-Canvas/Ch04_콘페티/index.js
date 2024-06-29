e658421abf7966fe96311d729d8b749c5c820656
islanderian <createdby.island@gmail.com>
2024-06-29 15:59:45 +0900

    04 - 05 콘페티 파티클 클래스 만들기2

--- Part2-Canvas/Ch04_콘페티/index.js
+++ Part2-Canvas/Ch04_콘페티/index.js
@@ -1,3 +1,5 @@
+import Particle from "./js/Particle.js";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const dpr = window.devicePixelRatio > 1 ? 2 : 1; // 최대 dpr 을 2로 지정(퍼포먼스 이슈)
@@ -5,6 +7,8 @@ let canvasWidth = innerWidth;
 let canvasHeight = innerHeight;
 const fps = 60;
 const interval = 1000 / fps;
+
+const particles = [];
 
 function init() {
   // window resize 될 때 canvas 크기 재조정
@@ -15,18 +19,21 @@ function init() {
   canvas.width = canvasWidth * dpr;
   canvas.height = canvasHeight * dpr;
   ctx.scale(dpr, dpr);
+
+  // confetti({
+  //   x: canvasWidth / 2,
+  //   y: canvasHeight / 2,
+  //   count: 10,
+  // });
+}
+
+function confetti({ x, y, count, deg }) {
+  for (let i = 0; i < count; i++) particles.push(new Particle(x, y, deg));
 }
 
 function render() {
   let now, delta;
   let then = Date.now();
-
-  const x = innerWidth / 2;
-  let y = innerHeight / 2;
-  let widthAlpha = 0;
-  const width = 50;
-  const height = 50;
-  let deg = 0.1;
 
   const frame = () => {
     requestAnimationFrame(frame);
@@ -36,26 +43,10 @@ function render() {
 
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    widthAlpha += 0.1;
-    deg += 0.1;
-    y += 1;
-
-    // 기본 rotate 를 하면 캔버스 (0, 0)을 중심축으로 회전함
-    ctx.translate(x + width, y + height); // 회전 시키고자 하는 객체의 중심축으로 이동
-    ctx.rotate(deg);
-    ctx.translate(-(x + width), -(y + height)); // 중심축 원위치
-
-    ctx.fillStyle = "red";
-    ctx.fillRect(
-      x,
-      y,
-      width * Math.cos(widthAlpha),
-      height * Math.sin(widthAlpha)
-    );
-    // ctx.fillRect(x, y, width, height);
-
-    ctx.resetTransform(); // rotate 값이 누적해서 더해지지 않도록
-    ctx.scale(dpr, dpr); // resetTransform 해주기 때문에 scale 도 같이 풀림. dpr 위해 다시 scaling
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].draw(ctx);
+    }
 
     then = now - (delta % interval);
   };
@@ -67,3 +58,11 @@ window.addEventListener("load", () => {
   render();
 });
 window.addEventListener("resize", init);
+window.addEventListener("click", () => {
+  confetti({
+    x: 0,
+    y: canvasHeight / 2,
+    count: 10,
+    deg: -50,
+  });
+});
