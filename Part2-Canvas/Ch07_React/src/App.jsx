47788f1c50e69523f5c0b36aac818a7d7fb4fc01
islanderian <createdby.island@gmail.com>
2024-07-18 01:25:08 +0900

    Ch07 - 03 UI작업하기1

--- Part2-Canvas/Ch07_React/src/App.jsx
+++ Part2-Canvas/Ch07_React/src/App.jsx
@@ -1,11 +1,29 @@
-import { useEffect } from "react";
-
 function App() {
-  useEffect(() => {
-    console.log("asdf");
-  }, []);
-
-  return <div className="App">hi</div>;
+  return (
+    <div className="app">
+      <section className="section-1">
+        <header>
+          <h1>Portfolio</h1>
+          <ul>
+            <li>isntagram</li>
+            <li>twitter</li>
+            <li>codepen</li>
+          </ul>
+        </header>
+        <main>
+          <div>canvas</div>
+        </main>
+      </section>
+      <section className="section-2">What is Lorem Ipsum?</section>
+      <section className="section-3">
+        <aside>
+          <div className="top">1914 translation by H. Rackham</div>
+          <div className="bottom"></div>
+        </aside>
+        <article></article>
+      </section>
+    </div>
+  );
 }
 
 export default App;
